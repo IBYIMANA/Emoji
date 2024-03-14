7721da6bcbcd13fdfe46eac10cfd18b138ee3ab4
IBYIMANA <filletteibyimana@gmail.com>
2024-03-14 12:41:00 +0200

    1

---
+++ emoj.js
@@ -0,0 +1,9 @@
+const emoji = ['👩🏼‍🤝‍🧑🏿','👩🏽‍🤝‍🧑🏻','👩🏼‍🤝‍🧑🏿','👩🏼‍🤝‍🧑🏾','👩🏼‍🤝‍🧑🏽','🦷','👅','👄','⛷','🤼‍♂️','👩‍❤️‍👩','🧠','💑','👨‍👩‍👧','🦑','🦐','🐠','🐟','🐚','🤒','😷','😡']
+const face1 = document.querySelector('#face');
+const buto = document.querySelector('button');
+buto.addEventListener("click", () => {
+    let round = Math.floor(Math.random() * 15);
+    let result = emoji[round];
+    face1.textContent = result;
+});
+console.log(emoji.length)
